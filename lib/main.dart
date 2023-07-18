2df418679c3d404c12b973bf362c7c79e3a019e8
Mateo Quintero <mateoquintero@tyba.com.co>
2023-07-18 18:24:05 -0500

    create and implement state providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_app/config/config.dart';
+import 'package:riverpod_app/presentation/providers/state_providers.dart';
 
 void main() => runApp(
       const ProviderScope(
@@ -14,11 +15,13 @@ class MyApp extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final appRouter = ref.watch(appRouterProvider);
+    final isDarkMode = ref.watch(isDarkModeProvider);
+
     return MaterialApp.router(
       title: 'Riverpod Providers',
       debugShowCheckedModeBanner: false,
       routerConfig: appRouter,
-      theme: AppTheme(isDarkmode: false).getTheme(),
+      theme: AppTheme(isDarkmode: isDarkMode).getTheme(),
     );
   }
 }
